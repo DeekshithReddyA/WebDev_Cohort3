8dc47697fea4867792294757b550d310c8f83586
DeekshithReddyA <deekshithreddy351@gmail.com>
2025-02-02 01:44:11 +0530

    Added routing

--- week15-second-brain-app/frontend/src/App.tsx
+++ week15-second-brain-app/frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { Dashboard } from "./components/pages/Dashboard";
 import { Signin } from "./components/pages/Signin";
 import { Signup } from "./components/pages/Signup";
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
 
 
 function App() {
 
   return (
-    <div className=''>
-      {/* <Dashboard /> */}
-      {/* <Signup /> */}
-      <Signin />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
