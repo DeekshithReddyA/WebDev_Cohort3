9a1b7a2a13d007f85746971735a3801a554a3193
DeekshithReddyA <deekshithreddy351@gmail.com>
2025-02-21 17:27:50 +0530

    File based routing

--- week19-NextJs/intro/app/page.tsx
+++ week19-NextJs/intro/app/page.tsx
@@ -19,7 +19,7 @@ function Todo({title , completed} : ITodo){
 
 export default async function Home() {
   const blogs  = await getBlogs();                // When this page is requested, in React, empty page is sent and then the data is fetched and rendered(client side rendering) whereas in Next, when the page is requested
-                                                  // The blogs are fetched and the dom manipulation is done and the html is sent(server side rendering) 
+                                                  // The blogs are fetched and the dom manipulation is done and the html is sent(server side rendering) . Looking at the network tab and the first html page, the blogs will be displayed
   return (
     <div>
       {blogs.map((blog: ITodo, index: number) => <Todo key={index} title={blog.title} completed={blog.completed} />)}
---
+++ week19-NextJs/intro/app/users/signup/page.tsx
@@ -0,0 +1,5 @@
+export default function Signup(){
+    return <div>
+        This is a user's singup page.
+    </div>
+}
