cfca3f83b398c2d736df46e3babdf1dab705e915
DeekshithReddyA <deekshithreddy351@gmail.com>
2025-02-21 13:56:51 +0530

    learnings

---
+++ week18-ORMs(Prisma)/Prisma/src/index.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const client = new PrismaClient();
+
+
+const createUser = async () => {
+    
+    await client.user.create({
+        data : {
+            username : "deekshith",
+            password : "deeks",
+            age : 20,
+            city : "Hyderabad"
+        }
+    });
+
+
+
+    await client.user.delete({
+        where : {
+            id : 1
+        }
+    });
+
+
+
+
+    await client.user.update({
+        where : {
+            id : 1
+        },
+        data : {
+            username : "deekshithReddy",
+            password : "deeks",
+            age : 21,
+            city : "Hyd"
+        }
+    });
+
+
+
+
+    await client.user.findFirst({
+        where : {
+            id : 1
+        },
+    });
+
+
+
+
+    await client.user.findFirst({
+        where : {
+            id : 1
+        },
+        select: {
+            username : true
+        }
+    });
+
+
+    await client.user.findFirst({
+        where : {
+            id : 1
+        },
+        include: {
+            todos : true
+        }
+    });
+};
+
+createUser();
