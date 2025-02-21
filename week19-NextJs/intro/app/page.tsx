851ef82c4f33083807c269d525b12a11b3608b2e
DeekshithReddyA <deekshithreddy351@gmail.com>
2025-02-21 17:24:47 +0530

    Showcase of server side rendering

--- week19-NextJs/intro/app/page.tsx
+++ week19-NextJs/intro/app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import axios from "axios";
+
+async function getBlogs(){
+  const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+  return response.data;
+}
+
+interface ITodo{
+  title: string;
+  completed: boolean;
+}
+
+function Todo({title , completed} : ITodo){
+  return <div>
+    {title} {completed ? "Done" : "Not done"}
+  </div>
+}
+
+
+export default async function Home() {
+  const blogs  = await getBlogs();                // When this page is requested, in React, empty page is sent and then the data is fetched and rendered(client side rendering) whereas in Next, when the page is requested
+                                                  // The blogs are fetched and the dom manipulation is done and the html is sent(server side rendering) 
   return (
     <div>
-
+      {blogs.map((blog: ITodo, index: number) => <Todo key={index} title={blog.title} completed={blog.completed} />)}
     </div>
   );
 }
